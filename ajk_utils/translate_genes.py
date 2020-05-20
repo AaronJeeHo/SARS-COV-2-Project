1a99174bf44545032e94a173e85df70525fc7c72
Aaron <aaron.jee.ho@gmail.com>
2020-05-20 01:51:25 -0700

    Created translate_genes.py
    - nuc_to_pro translates codons to amino acids

---
+++ ajk_utils/translate_genes.py
@@ -0,0 +1,24 @@
+def nuc_to_pro(seq):
+    translate = {
+        'TTT': 'F', 'TCT': 'S', 'TAT': 'Y', 'TGT': 'C',
+        'TTC': 'F', 'TCC': 'S', 'TAC': 'Y', 'TGC': 'C',
+        'TTA': 'L', 'TCA': 'S', 'TAA': '_', 'TGA': '_',
+        'TTG': 'L', 'TCG': 'S', 'TAG': '_', 'TGG': 'W',
+
+        'CTT': 'L', 'CCT': 'P', 'CAT': 'H', 'CGT': 'R',
+        'CTC': 'L', 'CCC': 'P', 'CAC': 'H', 'CGC': 'R',
+        'CTA': 'L', 'CCA': 'P', 'CAA': 'Q', 'CGA': 'R',
+        'CTG': 'L', 'CCG': 'P', 'CAG': 'Q', 'CGG': 'R',
+
+        'ATT': 'I', 'ACT': 'T', 'AAT': 'N', 'AGT': 'S',
+        'ATC': 'I', 'ACC': 'T', 'AAC': 'N', 'AGC': 'S',
+        'ATA': 'I', 'ACA': 'T', 'AAA': 'K', 'AGA': 'R',
+        'ATG': 'M', 'ACG': 'T', 'AAG': 'K', 'AGG': 'R',
+
+        'GTT': 'V', 'GCT': 'A', 'GAT': 'D', 'GGT': 'G',
+        'GTC': 'V', 'GCC': 'A', 'GAC': 'D', 'GGC': 'G',
+        'GTA': 'V', 'GCA': 'A', 'GAA': 'E', 'GGA': 'G',
+        'GTG': 'V', 'GCG': 'A', 'GAG': 'E', 'GGG': 'G'
+    }
+
+    return ''.join([translate[seq[i:i+3]] for i in range(0, len(seq), 3)])
